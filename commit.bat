200c2fcf3d83980a723d61924dfcf76bf8c6befa
xueshanlinghu <xueshanlinghu@xueshanlinghu.com>
2017-03-18 18:33:14 -0400

    quit

--- commit.bat
+++ commit.bat
@@ -1,20 +1,32 @@
+@ echo off
 REM 本文件撰写者：雪山凌狐
 REM 版本号：V1.0
 REM 时间：20170318
 REM 功能：自动把项目文件夹文件加入暂存并提交
 REM 文件编码：Utf-8无BOM格式编码
 
-@ echo off
 echo ----------------------git status before----------------------
 git status
+Echo.
 echo ----------------------git add file---------------------------
 git add .
+Echo.
 echo ----------------------git status now-------------------------
 git status
-echo --set commit message(you can type Ctrl+C to stop the bat-----
+Echo.
+echo --set commit message(you can type quit to stop the bat)------
 set /p commit_message=Please leave the message to commit:
+if %commit_message% == "quit" goto end
+if %commit_message% == "" set commit_message="no message update"
 set msg1=your commit message is:
 set msg2=-------
+Echo.
 echo %msg2%%msg1%%commit_message%%msg2%
 git commit -m %commit_message%
+Echo.
 echo ----------------------commit done----------------------------
+
+
+:end
+Echo.
+echo --------------------We stop the bat now----------------------
