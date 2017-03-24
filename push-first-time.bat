e3627d585b17381a6caf55fe610ea62c0e424d70
xueshanlinghu <xueshanlinghu@xueshanlinghu.com>
2017-03-24 11:09:25 -0400

    对两个bat文件在push阶段增加了同时提交到码云的代码

--- push-first-time.bat
+++ push-first-time.bat
@@ -1,8 +1,8 @@
 @ echo off
 rem 本文件撰写者：雪山凌狐
-rem 版本号：V1.0
-rem 时间：20170318
-rem 功能：自动把本地项目文件夹上传到云端GitHub，仅适用于第一次提交（只用一次，提交到云端空文件夹）
+rem 版本号：V1.1
+rem 时间：20170324
+rem 功能：自动把本地项目文件夹上传到云端GitHub和码云，仅适用于第一次提交（只用一次，提交到云端空文件夹）
 rem 文件编码：Utf-8无BOM格式编码
 rem 本文件适用于已经在云端创建好云端repo，在本地建立好公钥私钥，在云端添加好公钥的朋友们
 rem 使用方法：把文件里面的上传地址  git@github.com:xueshanlinghu/learn-git-and-github.git 改成你的即可
@@ -15,5 +15,15 @@ echo.
 echo --------------now we push the data to GitHub-----------------
 git push -u origin master
 git remote rm origin
+
+echo -------------------create remote git-osc----------------------
+git remote rm git-osc
+rem 这句代码的git@git.oschina.net:xueshanlinghu/learn-git-and-github.git改成你自己的远端地址哦~
+git remote add git-osc git@git.oschina.net:xueshanlinghu/learn-git-and-github.git
+echo.
+echo --------------now we push the data to git-osc-----------------
+git push -u git-osc master
+git remote rm git-osc
+
 echo.
 echo ----------------------push done------------------------------
